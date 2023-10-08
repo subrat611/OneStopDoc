9a58de012d20f4be1b2173e276bc5f2716a05d74
Subrat kumar jena <subratj611@gmail.com>
2023-10-09 00:18:03 +0530

    feat : roadmap + resources page add

---
+++ utils/roadmap.ts
@@ -0,0 +1,18 @@
+export const roadmaps = [
+    {
+        title: "JavaScript",
+        path: "javascript-roadmap"
+    },
+    {
+        title: "React.js",
+        path: "react-roadmap"
+    },
+    {
+        title: "MongoDB",
+        path: "mongodb-roadmap"
+    },
+    {
+        title: "Node.js",
+        path: "node-roadmap"
+    }
+]
