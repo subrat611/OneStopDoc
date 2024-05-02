a1055404d1c426d053cf4fa71140eb6f269b3c7a
Subrat kumar jena <subratj611@gmail.com>
2024-05-03 02:31:35 +0530

    vue.js pattern doc initial

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Frontend Interview Preparation Kit</span>,
+  logo: <span>Frontend Kit</span>,
   project: {
     link: "https://github.com/subrat611/OneStopDoc",
   },
   docsRepositoryBase: "https://github.com/subrat611/OneStopDoc",
   footer: {
-    text: "@2023 Subrat Jena, Surbhi Vandana",
+    text: "@2023 Subrat Jena",
   },
 };
 
